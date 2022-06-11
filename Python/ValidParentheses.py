2160876b0689c0463bb2fa2b38771b621b1a412d
mc25247 <61989852+mc25247@users.noreply.github.com>
2022-06-11 16:21:40 -0700

    Update ValidParentheses.py

--- Python/ValidParentheses.py
+++ Python/ValidParentheses.py
@@ -1,3 +1,27 @@
+'''
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+1.Open brackets must be closed by the same type of brackets.
+2.Open brackets must be closed in the correct order.
+
+EX1:
+Input: s = "()"
+Output: true
+
+EX2:
+Input: s = "()[]{}"
+Output: true
+
+EX3:
+Input: s = "(]"
+Output: false
+
+Constraints:
+- 1 <= s.length <= 104
+- s consists of parentheses only '()[]{}'.
+'''    
 class Solution(object):
     def isValid(self, s):
         """
